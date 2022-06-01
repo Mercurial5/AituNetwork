54966c23c76ada4b187f30d904e9c4395510900d
mercurial <dias.nespayev@gmail.com>
2022-06-01 11:02:07 +0600

    chore: add `get` methods

--- aituNetwork/models/Comments.py
+++ aituNetwork/models/Comments.py
@@ -7,3 +7,7 @@ class Comments(db.Model):
     user_id = db.Column(db.Integer, nullable=False, index=True)
     text = db.Column(db.Text, nullable=False)
     created = db.Column(db.DATETIME, nullable=False, default=datetime.now)
+
+    @staticmethod
+    def get(comment_id: int):
+        return Comments.query.get(comment_id)
--- aituNetwork/models/PostComments.py
+++ aituNetwork/models/PostComments.py
@@ -5,3 +5,7 @@ class PostComments(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     post_id = db.Column(db.Integer, nullable=False, index=True)
     comment_id = db.Column(db.Integer, nullable=False, index=True)
+
+    @staticmethod
+    def get(post_id: int):
+        return PostComments.query.filter_by(post_id=post_id).all()
