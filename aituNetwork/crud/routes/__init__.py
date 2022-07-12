56abbd1fdce7a48cdd56c31873168044947c3334
mercurial <dias.nespayev@gmail.com>
2022-07-12 09:09:06 +0600

    ref: move create & delete comment

--- aituNetwork/crud/routes/__init__.py
+++ aituNetwork/crud/routes/__init__.py
@@ -1,2 +1,3 @@
 from aituNetwork.crud.routes import friends
 from aituNetwork.crud.routes import posts
+from aituNetwork.crud.routes import comments
--- aituNetwork/users/routes.py
+++ aituNetwork/users/routes.py
@@ -213,39 +213,3 @@ def delete_user(user_id: int):
     db.session.commit()
 
     return 'User was deleted'
-
-
-
-
-
-@users.route('/add/comment', methods=['POST'])
-@auth_required
-def add_comment():
-    post_id = int(request.form.get('post_id'))
-    author_id = session['user'].id
-    text = request.form.get('text')
-
-    comment = Comments.create_comment(author_id, text)
-    PostComments.add_comment_to_post(post_id, comment.id)
-
-    return 'ok'
-
-
-@users.route('/delete/comment')
-@auth_required
-def delete_comment():
-    comment_id = request.values.get('comment_id')
-
-    user = session['user']
-    comment = Comments.get(comment_id)
-
-    if comment is None:
-        return 'Comment not found'
-
-    if comment.user_id != user.id and not Admins.is_admin(user.id):
-        return 'You don\'t have an access'
-
-    Comments.delete_comment(comment_id)
-    PostComments.delete_comment_from_post(comment_id)
-
-    return redirect(url_for('main.home'))
